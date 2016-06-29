eb595b862fab4fbef69c358f28b4f58dec2c15e9
Shoeb Amin <ShoebAmin@Ss-MacBook-Pro.local>
2016-06-29 19:58:14 +0900

    Project initialised

---
+++ SampleWeatherApp/SampleWeatherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleWeatherApp
+//
+//  Created by Shoeb Amin on 6/29/16.
+//  Copyright © 2016 SiliconOrchard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
