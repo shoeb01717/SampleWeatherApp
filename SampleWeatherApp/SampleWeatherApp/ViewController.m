9e73a1bf25815d2b70bc34b1f2a2c93ee1cc081a
Shoeb Amin <ShoebAmin@Ss-MacBook-Pro.local>
2016-07-07 15:21:33 +0900

    humidity added wind direction and speed added

--- SampleWeatherApp/SampleWeatherApp/ViewController.m
+++ SampleWeatherApp/SampleWeatherApp/ViewController.m
@@ -16,6 +16,11 @@
 @property (strong, nonatomic) IBOutlet UILabel *currentLocationLabel;
 @property (strong, nonatomic) IBOutlet UIImageView *weatherIconImageView;
 @property (strong, nonatomic) IBOutlet UILabel *temparatureLabel;
+@property (strong, nonatomic) IBOutlet UILabel *lowTemparatureLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *windDirectionImageView;
+@property (strong, nonatomic) IBOutlet UILabel *windSpeedLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *humidityLabel;
 
 @end
 
@@ -31,18 +36,37 @@
     } cityName:^(NSString *cityName) {
         NSLog(@"CityName: %@",cityName);
         self.currentLocationLabel.text = cityName;
-        [self settingUpUIWithWeather];
+        [self settingUpUIWithCityName:[[LocationManager sharedManager] getCityName]];
     } onFailure:^(NSError *error) {
         
     }];
 }
 
 
--(void)settingUpUIWithWeather{
+-(void)settingUpUIWithCityName:(NSString *) cityName {
 
-    [[YWeatherAPI sharedManager] todaysForecastForLocation:[[LocationManager sharedManager] getCityName]success:^(NSDictionary *result) {
+    [[YWeatherAPI sharedManager] todaysForecastForLocation:cityName success:^(NSDictionary *result) {
         [self.weatherIconImageView setImage:[UIImage imageNamed:[result objectForKey:kYWAShortDescription]]];
-        self.temparatureLabel.text = [result objectForKey:kYWAHighTemperatureForDay];
+        self.temparatureLabel.text = [NSString stringWithFormat:@"%@°F",[result objectForKey:kYWAHighTemperatureForDay]];
+        self.lowTemparatureLabel.text = [NSString stringWithFormat:@"%@°F",[result objectForKey:kYWALowTemperatureForDay]];
+    } failure:^(id response, NSError *error) {
+        
+    }];
+    
+    [[YWeatherAPI sharedManager] humidityForLocation:cityName success:^(NSDictionary *result) {
+        self.humidityLabel.text = [NSString stringWithFormat:@"%@%%",[result objectForKey:kYWAHumidity]];
+    } failure:^(id response, NSError *error) {
+        
+    }];
+    
+    [[YWeatherAPI sharedManager] windDirectionForLocation:cityName success:^(NSDictionary *result) {
+        self.windDirectionImageView.transform = CGAffineTransformMakeRotation(-[[result objectForKey:kYWAWindDirectionInDegrees] floatValue]);
+    } failure:^(id response, NSError *error) {
+        
+    }];
+    
+    [[YWeatherAPI sharedManager] windSpeedForLocation:cityName success:^(NSDictionary *result) {
+        self.windSpeedLabel.text = [NSString stringWithFormat:@"%@ MPH",[result objectForKey:kYWAWindSpeedInMPH]];
     } failure:^(id response, NSError *error) {
         
     }];
